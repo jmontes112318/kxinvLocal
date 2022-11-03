0c709812587766f197afb94bd5617114c51bb2aa
jmontes <jhon.montes.mt@usa.edu.co>
2022-11-03 17:59:55 -0500

    consultaEstante modificado

--- ConsultaEstantes.php
+++ ConsultaEstantes.php
@@ -11,14 +11,21 @@ if (empty($_POST['bodegaRegEst']) && empty($_POST['pasilloregEst'])) {
 
     $query = "SELECT DISTINCTROW pasillo,estante FROM `productos` WHERE bodega ='$bodResEs'";
     $result = mysqli_query($connection, $query);
+} elseif (!empty($_POST['bodegaRegEst']) && !empty($_POST['pasilloregEst'])) {
+
+    $bodResEst = $_POST['bodegaRegEst'];
+    $pasResEst = $_POST['pasilloRegEst'];
+
+    $query = "SELECT DISTINCTROW pasillo,estante FROM `productos` WHERE bodega ='$bodResEst' AND pasillo ='$pasResEst'";
+    $result = mysqli_query($connection, $query);
 }
 
 
 
 
-$query = "SELECT DISTINCTROW pasillo,estante FROM `productos` WHERE bodega ='501'";
-$result = mysqli_query($connection, $query);
-$filas = mysqli_num_rows($result);
+// $query = "SELECT DISTINCTROW pasillo,estante FROM `productos` WHERE bodega ='501'";
+// $result = mysqli_query($connection, $query);
+// $filas = mysqli_num_rows($result);
 
 if (!$result) {
     die("Error");
